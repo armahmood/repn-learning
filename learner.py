f4b8e24c9f23d79889fe524ff9f32c1d33c31fc0
taapasX28 <iamtaapas@iitkgp.ac.in>
2020-04-18 04:51:50 +0530

    LTU threshold fixed

--- learner.py
+++ learner.py
@@ -2,32 +2,31 @@ import numpy as np
 import torch
 from torch import nn, optim
 import matplotlib.pyplot as plt
+from random import choice
 from torch.nn.parameter import Parameter
 
 T = 30000
 nbin = 200
 n_inp = 10
 n_tl1 = 20
-def calculate_threshold(input, weights):
+def calculate_threshold(weights):
   S_i = 0
   for weight in weights:
     if weight<0:
       S_i += 1
-  threshold = len(input)*0.6 - S_i
+  threshold = len(weights)*0.6 - S_i
   return threshold
 
 def ltu(input, weights):
   i = 0
-  feature_vector = []
-  for i in range(n_tl1):
-    tau_i = calculate_threshold(input, weights[i])
-    x = weights[i]
-    if float(torch.mm(x, input)) < tau_i:
-      feature_vector.append(0.0)
+  for i in range(len(input)):
+    tau_i = calculate_threshold(weights[i])
+    if input[i] < tau_i:
+      input[i] = 0.0
     else: 
-      feature_vector.append(1.0)
+      input[i] = 1.0
     i=i+1
-  return torch.from_numpy(feature_vector)
+  return input
 
 class LTU(nn.Module):
   def __init__(self, n_inp, n_tl1):
@@ -42,16 +41,17 @@ def input_weight_init(size):
   inp_weights = np.random.choice(weight_choice, size)
   return torch.from_numpy(inp_weights)
 
+activation_function = LTU(n_inp, n_tl1)
 torch.manual_seed(0)
-tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), LTU(n_tl1, n_tl1), nn.Linear(n_tl1, 1))
+tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), activation_function, nn.Linear(n_tl1, 1))
+
 with torch.no_grad():
-    tnet[2].weight *= 100
-    tnet[0].weight *= 1
+    tnet[1].weight.data = tnet[0].weight.data
 lossfunc = nn.MSELoss()
 
 for n_l1 in [10, 30, 100, 1000]:
     torch.manual_seed(1000)
-    net = nn.Sequential(nn.Linear(n_inp, n_l1), LTU(n_tl1, n_tl1), nn.Linear(n_l1, 1))
+    net = nn.Sequential(nn.Linear(n_inp, n_l1), nn.Sigmoid(), nn.Linear(n_l1, 1))
     with torch.no_grad():
         net[2].weight *= 0
         net[2].bias *= 0
@@ -61,7 +61,7 @@ for n_l1 in [10, 30, 100, 1000]:
     losses = []
     for t in range(T):
         inp = torch.rand(n_inp)
-        target = tnet(inp) + torch.randn(1)
+        target = tnet(inp)+ torch.randn(1)
         pred = net(inp)
         loss = lossfunc(target, pred)
         losses.append(loss.item())

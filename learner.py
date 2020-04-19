56d19a45bbade1f2f9fb26b7a94adb958c887234
taapasX28 <iamtaapas@iitkgp.ac.in>
2020-04-20 00:32:43 +0530

    Fixed initialization of output layer

--- learner.py
+++ learner.py
@@ -10,6 +10,7 @@ nbin = 200
 n_inp = 10
 n_tl1 = 20
 def calculate_threshold(weights):
+  """Calculates LTU threshold according to weights"""
   S_i = 0
   for weight in weights:
     if weight<0:
@@ -18,6 +19,7 @@ def calculate_threshold(weights):
   return threshold
 
 def ltu(input, weights):
+  """LTU logic"""
   i = 0
   for i in range(len(input)):
     tau_i = calculate_threshold(weights[i])
@@ -29,6 +31,7 @@ def ltu(input, weights):
   return input
 
 class LTU(nn.Module):
+  """LTU activation function"""
   def __init__(self, n_inp, n_tl1):
     super().__init__()
     self.weight = Parameter(torch.Tensor(n_tl1, n_inp))
@@ -37,6 +40,7 @@ class LTU(nn.Module):
     return ltu(input, self.weight)
 
 def input_weight_init(inp, out):
+  """Function for initializing input weight"""
   weight_choice = [1,-1]
   inp_weights = np.random.choice(weight_choice, (out,inp))
   return torch.from_numpy(inp_weights).float()
@@ -46,7 +50,10 @@ torch.manual_seed(0)
 tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), activation_function, nn.Linear(n_tl1, 1))
 
 with torch.no_grad():
+    #Input weights initialized with +1/-1
     tnet[0].weight.data = input_weight_init(n_inp, n_tl1)
+    #Output layer weights initialized with N(0,1)
+    torch.nn.init.normal_(tnet[2].weight, mean=0.0, std=1.0)
     tnet[1].weight = tnet[0].weight
 lossfunc = nn.MSELoss()
 

c412adb7982aab8b1ffee298b29708e7b2505062
Taapas Agrawal <IAMTAAPAS@iitkgp.ac.in>
2020-05-22 23:10:47 +0530

    Restructured code for average error (#3)
    * Restructured code for average error
    
    * Fixed seed issue and added argument for seeds
    
    * Added manual seeds for random variables
    
    * user initialized seed for target net
    
    * Final seed fix
    
    * seed fix
    
    * Code cleanup and file save for multiple t_nets
    
    * added output weight norm title for graphs
    
    Co-authored-by: Rupam Mahmood <ashique@ualberta.ca>

--- learner.py
+++ learner.py
@@ -4,11 +4,11 @@ from torch import nn, optim
 import matplotlib.pyplot as plt
 from random import choice
 from torch.nn.parameter import Parameter
+import progressbar
+import argparse
+import os
+import sys
 
-T = 30000
-nbin = 200
-n_inp = 10
-n_tl1 = 20
 def calculate_threshold(weights):
   """Calculates LTU threshold according to weights"""
   S_i = len(weights[weights<0])
@@ -50,50 +50,132 @@ def update_lr(optimizer,lr):
         param_group['lr'] = lr
     return optimizer
 
-activation_function = LTU(n_inp, n_tl1)
-torch.manual_seed(0)
-tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), activation_function, nn.Linear(n_tl1, 1))
-
-with torch.no_grad():
+def initialize_target_net(n_inp, n_tl1, seed_num_target):
+  """Initializes target network"""
+  torch.manual_seed(seed_num_target)
+  activation_function = LTU(n_inp, n_tl1)
+  tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), activation_function, nn.Linear(n_tl1, 1))
+  with torch.no_grad():
     #Input weights initialized with +1/-1
+    np.random.seed(seed_num_target)
     tnet[0].weight.data = input_weight_init(n_inp, n_tl1)
     #Output layer weights initialized with N(0,1)
     torch.nn.init.normal_(tnet[2].weight, mean=0.0, std=1.0)
     tnet[1].weight = tnet[0].weight
-lossfunc = nn.MSELoss()
+  return tnet
 
+def initialize_learning_net(n_inp, n_l1, seed_num):
+  """Initializes learning network"""
+  torch.manual_seed(seed_num)
+  activation_function_ = LTU(n_inp, n_l1)
+  net = nn.Sequential(nn.Linear(n_inp, n_l1), activation_function_, nn.Linear(n_l1, 1))
+  with torch.no_grad():
+    np.random.seed(seed_num)
+    net[0].weight.data = input_weight_init(n_inp, n_l1)
+    net[1].weight = net[0].weight
+    torch.nn.init.zeros_(net[2].weight)
+    torch.nn.init.zeros_(net[2].bias)
+  return net
 
-for n_l1 in [10, 30, 100, 1000]:
-    torch.manual_seed(1000)
-    activation_function_ = LTU(n_inp, n_l1)
-    net = nn.Sequential(nn.Linear(n_inp, n_l1), activation_function_, nn.Linear(n_l1, 1))
-    with torch.no_grad():
-        net[0].weight.data = input_weight_init(n_inp, n_l1)
-        net[1].weight = net[0].weight
-        torch.nn.init.zeros_(net[2].weight)
-        torch.nn.init.zeros_(net[2].bias)
-    
-    sgd = optim.SGD(net[2:].parameters(), lr = 0.0)
-    torch.manual_seed(2000)
-    losses = []
-    sample_average = 0.0
+def run_experiment(n_inp, n_tl1, T, n_l1, seed_num, target_seed):
+  """Experiment with different number of features without search"""
+  tnet = initialize_target_net(n_inp, n_tl1, target_seed)
+  lossfunc = nn.MSELoss()
+  net = initialize_learning_net(n_inp, n_l1, seed_num)
+  sgd = optim.SGD(net[2:].parameters(), lr = 0.0)
+  torch.manual_seed(seed_num + 2000)
+  losses = []
+  sample_average = 0.0
+  with progressbar.ProgressBar(max_value=T) as bar:
     for t in range(T):
-        inp = torch.rand(n_inp)
-        target = tnet(inp) + torch.randn(1)
-        pred = net(inp)
-        loss = lossfunc(target, pred)
-        losses.append(loss.item())
-        net.zero_grad()
-        loss.backward()
-        #Evaluate step size parameter
-        f_out = net[1].out_features
-        sample_average = (sample_average *t + (f_out.norm()**2).item())/(t+1)
-        step_size_param = 0.1/sample_average
-        sgd = update_lr(sgd,step_size_param)
-        sgd.step()
-        
-    losses = np.array(losses)
-    bin_losses = losses.reshape(T//nbin, nbin).mean(1)
-    plt.plot(range(0, T, nbin), bin_losses, label=n_l1)
-plt.legend()
-plt.show()
+      inp = torch.rand(n_inp)
+      target = tnet(inp) + torch.randn(1)
+      pred = net(inp)
+      loss = lossfunc(target, pred)
+      losses.append(loss.item())
+      net.zero_grad()
+      loss.backward()
+      #Evaluate step size parameter
+      f_out = net[1].out_features
+      sample_average = (sample_average *t + (f_out.norm()**2).item())/(t+1)
+      step_size_param = 0.1/sample_average
+      sgd = update_lr(sgd,step_size_param)
+      sgd.step()
+      bar.update(t)
+  losses = np.array(losses)
+  return losses
+
+def main():
+  parser = argparse.ArgumentParser(description="Test framework")
+  parser.add_argument("-e", "--examples", type=int, default=30000,
+                      help="no of examples to learn on")
+  parser.add_argument("-n", "--runs", type=int, default=1,
+                      help="number of runs")
+  parser.add_argument("-i", "--input_size", type=int, default=10,
+                      help="Input dimension")
+  parser.add_argument("-f", "--features",  nargs='+', type=int, default=[100, 300, 1000],
+                      help="Number of dimension(pass multiple)")
+  parser.add_argument("-o", "--save", type=bool, default=False,
+                      help="Saves the output graph")
+  parser.add_argument("-s", "--seeds",  nargs='+', type=int, default=[1],
+                      help="seeds in case of multiple runs")
+  parser.add_argument("-t", "--target_seed", type=int, default=4000,
+                      help="seed for choice of target net")
+  args = parser.parse_args()
+  T = args.examples
+  n = args.runs 
+  nbin = 200
+  n_inp = args.input_size
+  n_tl1 = 20
+  n_feature = args.features
+  n_seed = args.seeds
+  t_seed = args.target_seed
+
+  try:
+    path = "output/out_" + str(t_seed)+".png"
+    assert not os.path.exists(path)
+  except:
+    print("Experiment results already exist")
+    sys.exit(1)
+
+  try:
+    assert t_seed not in n_seed
+  except:
+    print("Error: t_seed has to be different than n_seed")
+    sys.exit(1)
+
+  if (len(n_seed)!=n):
+    print("Insuffcient number of seeds")
+    return
+
+  for nl_1 in n_feature:
+    net_loss = 0
+    print("No of Features:", nl_1)
+    for l in range(n):
+      print("Run:", l+1)
+      net_loss = net_loss + run_experiment(n_inp, n_tl1, T, nl_1, n_seed[l], t_seed)
+    net_loss = net_loss/n
+    bin_losses = net_loss.reshape(T//nbin, nbin).mean(1)
+    plt.plot(range(0, T, nbin), bin_losses, label=nl_1)
+  tnet = initialize_target_net(n_inp, n_tl1, t_seed)
+  norm_out = tnet[-1].weight.norm().data
+  norm_out = format(float(norm_out), '.4f')
+  title = "Output weight norm of t-net: " + str(norm_out) 
+  plt.suptitle(title, fontsize=13)
+  axes = plt.axes()
+  axes.set_ylim([1.0, 3.5])
+  plt.legend()
+
+  if args.save:
+    try:
+      assert os.path.exists("output/")
+    except:
+      os.makedirs('output/') 
+    filename = "output/out_" + str(t_seed)
+    plt.savefig(filename)
+  else:
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

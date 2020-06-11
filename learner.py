2606d496378a797e3703dc32f41a1bef1e2b656b
armahmood <ashique@ualberta.ca>
2020-06-11 13:10:53 -0600

    random objects

--- learner.py
+++ learner.py
@@ -10,6 +10,14 @@ import os
 import sys
 from fractions import Fraction
 
+
+"""
+Three sources of randomness.
+1. Target network
+2. Learning network
+3. Data generation
+"""
+
 def calculate_threshold(weights):
   """Calculates LTU threshold according to weights"""
   S_i = len(weights[weights<0])
@@ -39,40 +47,33 @@ class LTU(nn.Module):
     self.out_features = input.clone()
     return input
 
-def input_weight_init(inp, out):
-  """Function for initializing input weight"""
-  weight_choice = [1,-1]
-  inp_weights = np.random.choice(weight_choice, (out,inp))
-  return torch.from_numpy(inp_weights).float()
-
 def update_lr(optimizer,lr):
     """Scheduler to update learning rate at every iteration"""
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     return optimizer
 
-def initialize_target_net(n_inp, n_tl1, seed_num_target):
+def initialize_target_net(n_inp, n_tl1, tgen, seed_num_target):
   """Initializes target network"""
-  torch.manual_seed(seed_num_target)
   activation_function = LTU(n_inp, n_tl1)
-  tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=True), activation_function, nn.Linear(n_tl1, 1))
+  tnet = nn.Sequential(nn.Linear(n_inp, n_tl1, bias=False), activation_function, nn.Linear(n_tl1, 1))
   with torch.no_grad():
     #Input weights initialized with +1/-1
-    np.random.seed(seed_num_target)
-    tnet[0].weight.data = input_weight_init(n_inp, n_tl1)
+    tgen.manual_seed(seed_num_target)
+    tnet[0].weight.data = (torch.randint(0, 2, tnet[0].weight.data.shape, generator=tgen)*2-1).float()  ### 1
     #Output layer weights initialized with N(0,1)
-    torch.nn.init.normal_(tnet[2].weight, mean=0.0, std=1.0)
+    tnet[2].weight.data = torch.randn(tnet[2].weight.data.shape, generator=tgen)  ### 1
+    tnet[2].bias.data = torch.randn(tnet[2].bias.data.shape, generator=tgen)  ### 1
     tnet[1].weight = tnet[0].weight
   return tnet
 
-def initialize_learning_net(n_inp, n_l1, seed_num):
+def initialize_learning_net(n_inp, n_l1, lgen, seed_num):
   """Initializes learning network"""
-  torch.manual_seed(seed_num)
   activation_function_ = LTU(n_inp, n_l1)
-  net = nn.Sequential(nn.Linear(n_inp, n_l1), activation_function_, nn.Linear(n_l1, 1))
+  net = nn.Sequential(nn.Linear(n_inp, n_l1, bias=False), activation_function_, nn.Linear(n_l1, 1))
   with torch.no_grad():
-    np.random.seed(seed_num)
-    net[0].weight.data = input_weight_init(n_inp, n_l1)
+    lgen.manual_seed(seed_num)
+    net[0].weight.data = (torch.randint(0, 2, net[0].weight.data.shape, generator=lgen)*2-1).float()  ### 2
     net[1].weight = net[0].weight
     torch.nn.init.zeros_(net[2].weight)
     torch.nn.init.zeros_(net[2].bias)
@@ -80,17 +81,19 @@ def initialize_learning_net(n_inp, n_l1, seed_num):
 
 def run_experiment(n_inp, n_tl1, T, n_l1, seed_num, target_seed):
   """Experiment with different number of features without search"""
-  tnet = initialize_target_net(n_inp, n_tl1, target_seed)
+  tgen = torch.Generator()
+  tnet = initialize_target_net(n_inp, n_tl1, tgen, target_seed)
   lossfunc = nn.MSELoss()
-  net = initialize_learning_net(n_inp, n_l1, seed_num)
+  lgen = torch.Generator()
+  net = initialize_learning_net(n_inp, n_l1, lgen, seed_num)
   sgd = optim.SGD(net[2:].parameters(), lr = 0.0)
-  torch.manual_seed(seed_num + 2000)
+  dgen = torch.Generator().manual_seed(seed_num + 2000)
   losses = []
   sample_average = 0.0
   with progressbar.ProgressBar(max_value=T) as bar:
     for t in range(T):
-      inp = torch.rand(n_inp)
-      target = tnet(inp) + torch.randn(1)
+      inp = torch.rand(n_inp, generator=dgen)  ### 3
+      target = tnet(inp) + torch.randn(1, generator=dgen)  ### 3
       pred = net(inp)
       loss = lossfunc(target, pred)
       losses.append(loss.item())
@@ -133,8 +136,8 @@ def run_experiment_search(n_inp, n_tl1, T, n_l1, seed_num, target_seed):
 
   with progressbar.ProgressBar(max_value=T) as bar:
     for t in range(T):
-      inp = torch.rand(n_inp)
-      target = tnet(inp) + torch.randn(1)
+      inp = torch.rand(n_inp)  ### 3
+      target = tnet(inp) + torch.randn(1)  ### 3
       neck = net[:2](inp)
       pred = net[2:](neck)
       loss = lossfunc(target, pred)
@@ -155,8 +158,8 @@ def run_experiment_search(n_inp, n_tl1, T, n_l1, seed_num, target_seed):
           for _ in range(itr):
             weak_node_i = torch.argmin(utils)
             weight_choice = [1.0,-1.0]
-            net[0].weight[weak_node_i] = torch.from_numpy(np.random.choice(weight_choice, (net[0].weight.size()[1],)))
-            net[0].bias[weak_node_i] = torch.randn(1)
+            net[0].weight[weak_node_i] = torch.from_numpy(np.random.choice(weight_choice, (net[0].weight.size()[1],)))  ### 2
+            net[0].bias[weak_node_i] = torch.randn(1)  ### 2
             net[2].weight[0][weak_node_i] = 0.0
             utils[weak_node_i] = torch.median(utils)
             ages[weak_node_i] = 0
